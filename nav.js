c7be393e0b6023d9ba185d9d533bb634c20c99a4
promise <promiseniyi@gmail.com>
2023-05-23 22:27:01 +0100

    nav toglgle icon fixed

--- nav.js
+++ nav.js
@@ -1,16 +1,41 @@
 const primaryNav = document.querySelector(".navigate");
+const primNav = document.querySelector(".nav-togglecancel");
+
 const navToggle = document.querySelector(".nav-toggle");
+const navToggletwo = document.querySelector(".nav-togglecancel");
 
 navToggle.addEventListener('click', () =>{
  const visibility = primaryNav.getAttribute('data-visible')
+ 
 
  if (visibility === "false"){
   primaryNav.setAttribute("data-visible", true);
   navToggle.setAttribute('aria-expanded', true );
+  primNav.setAttribute("data-visible", true);
+
  }else if(visibility=== "true"){
   primaryNav.setAttribute("data-visible", false);
    navToggle.setAttribute('aria-expanded', false );
+   primNav.setAttribute("data-visible", false);
  }
 
 
 });  
+
+navToggletwo.addEventListener('click', () =>{
+  const visibility = primaryNav.getAttribute('data-visible')
+  
+ 
+  if (visibility === "false"){
+   primaryNav.setAttribute("data-visible", true);
+   primNav.setAttribute("data-visible", true);
+   
+ 
+  }else if(visibility=== "true"){
+   primaryNav.setAttribute("data-visible", false);
+   primNav.setAttribute("data-visible", false);
+   
+  }
+ 
+ 
+ });  
